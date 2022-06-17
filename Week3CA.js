b44412f9b5530efe62eb44b3308b1fcb28c1dfd7
Sbilavanh <bilavanh.souphalak@gmail.com>
2022-06-16 18:36:38 -0700

    week3 code assignment

---
+++ Week3CA.js
@@ -0,0 +1,42 @@
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+
+console.log(ages);
+
+let age1;
+let age2;
+
+ages.push(69)
+
+for (i = 0; i < ages.length; i++) {
+    if (i == 0) {
+        age1 = ages[i];
+    }
+    if (i == ages.length - 1) {
+        age2 = ages[i];
+    }
+}
+
+console.log(age2 - age1);
+
+let sum = 0;
+
+for (let average of ages) {
+    sum += average;
+}
+
+average = sum / ages.length;
+
+console.log(average);
+
+// let sum;
+// function average(ages) {
+//     for (i = 0; i < ages.length; i++) {
+//         sum += ages[i];
+//     }
+//     return i / ages.length;
+// }
+// console.log(ages);
+
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
+
+console.log(names)
